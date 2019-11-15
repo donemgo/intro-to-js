41e8ee639894fe846feb027d44ce567378395ef6
bobbylemm <chidozienwoga@gmail.com>
2019-11-15 19:42:44 +0100

    ensure we talk about comparisons operators and expressions

---
+++ opexco/index.js
@@ -0,0 +1,12 @@
+// the operators we have have in JS are going to be familiar to you
+// + - * / %
+
+const five = 2 + 3;
+// we have two special kinds of operators, ++ -- . these operators increments and decrements values respectively.
+
+let x = 5;
+x = x + 1;
+// or
+x++;
+
+//variables can also store the value of expressions, e.g 4 * 6 is an expression,  which is noce because we can reuse the results of these expressions for later without repeating ourselves
