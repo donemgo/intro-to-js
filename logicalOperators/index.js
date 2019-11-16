ae143dc7db7d9f05172b96a9a6355e04fe2b9510
bobbylemm <chidozienwoga@gmail.com>
2019-11-16 07:29:40 +0100

    ensure we complete logical operators

--- logicalOperators/index.js
+++ logicalOperators/index.js
@@ -1,12 +1,12 @@
 // logical operators are also a different type of operators, but what they help us achieve is unique when compared to the other comparators
 
-// || OR . the "or" statement simply looks at two values and checks to see if one of them is true, if one is true it is satisfied
+// || OR . the "or" statement simply looks at two values and checks to see if one of them is true, if one is true it is satisfied, OR returns the first true it can find, if not it returns false.
 
 const me = true;
 const you = false;
 
 me || you || console.log('I passed o');
-// && AND . the "and" statement, unlike the OR is kind of stricter, it wants and needs all values to be true before it can allow you through. these operators return boolean values at the end
+// && AND . the "and" statement, unlike the OR is kind of stricter, it wants and needs all values to be true before it can allow you through. and would return the last true statement if it successfull, if not it returns false
 const me = true;
 const you = true;
 
