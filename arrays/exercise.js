890c652ed294737ccc657a05559dfd3f35411c06
bobbylemm <chidozienwoga@gmail.com>
2019-11-16 07:58:26 +0100

    arrays and manipulating arrays

---
+++ arrays/exercise.js
@@ -0,0 +1,7 @@
+// create a variable called favoriteCar  and it should be assign a string concatenation of
+// "hello there this is my favorite car ____"
+const cars = ['g wagon', 'mercedes', 'toyota', 'bmw', 'bentely'];
+
+//  Now add two new cars to the array, and also remove two cars. at the end of this operation, the first item in the array should be toyota
+
+// change the second car in the array to a "bentaga"
